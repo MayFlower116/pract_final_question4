892a8f112981fd6e6b6acbbb4392b30355340168
MayFlower116 <elijahjkrieger@gmail.com>
2021-11-22 17:12:19 -0800

    only grayscrale left

--- starter 4/IOhandler.js
+++ starter 4/IOhandler.js
@@ -8,10 +8,13 @@
  *
  */
 
+// const { Resolver } = require("dns");
+// const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
   path = require("path");
+
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -20,7 +23,17 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+
+const unzip = (pathIn, pathOut) => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(pathIn)
+    .pipe(unzipper.Extract({ path: pathOut }));
+    resolve()
+  }).then(() => {
+    console.log('Extraction Done')
+    resolve()
+  })
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -28,7 +41,21 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, data) => {
+      new Promise((resolve, reject) => {
+        pngArr = []
+        for (let i = 0; i < data.length; i++){
+          if (path.extname(`${dir}/${data[i]}`).toLowerCase() == '.png') {
+            pngArr.push(data[i])
+          }
+        }
+        resolve(pngArr)
+      }).then((pngArr) => resolve(pngArr))
+    });
+  });
+};
 
 /**
  * Description: Read in png file by given pathIn,
@@ -38,7 +65,34 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = (pathIn, pathOut) => {
+  // see example, barely need any changes to work
+  // DO NOT CHANGE filter type --- not allowed
+  // change rgb to make grayscale
+  // see instructions for "simple algorithms you can use for converting to grayscale"
+  fs.createReadStream(pathIn)
+  .pipe(
+    new PNG({
+      filterType: 4,
+    })
+  )
+  .on("parsed", function () {
+    for (var y = 0; y < this.height; y++) {
+      for (var x = 0; x < this.width; x++) {
+        var idx = (this.width * y + x) << 2;
+ 
+        // invert color
+        this.data[idx] = this.data[idx]/3;
+        this.data[idx + 1] = this.data[idx + 1]/3;
+        this.data[idx + 2] = this.data[idx + 2]/3;
+      }
+    }
+ 
+    this.pack().pipe(fs.createWriteStream(pathOut));
+  });
+};
+
+
 
 module.exports = {
   unzip,
--- starter 4/main.js
+++ starter 4/main.js
@@ -12,3 +12,15 @@ const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+  IOhandler.unzip('./starter 4/myfile.zip', './starter 4/unzipped').then(
+    IOhandler.readDir('./starter 4/unzipped')
+    // .then((png_array) => console.log(png_array))
+    .then((png_array) => {
+      console.log('Files Sorted')
+      png_array.forEach(image => {
+        IOhandler.grayScale(`./starter 4/unzipped/${image}`, './starter 4/gray_scaled')
+      });
+    }
+  )
+)
