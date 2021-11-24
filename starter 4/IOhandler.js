e149c4cbf414f5bfcbce289745223191a6988f8b
MayFlower116 <elijahjkrieger@gmail.com>
2021-11-23 22:56:54 -0800

    i give up
    I've already wasted 3 hours on grayscale, i have better garbage to do for this class and others.

--- starter 4/IOhandler.js
+++ starter 4/IOhandler.js
@@ -10,6 +10,8 @@
 
 // const { Resolver } = require("dns");
 // const { resolve } = require("path");
+// const { resolve } = require("path");
+const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -29,10 +31,10 @@ const unzip = (pathIn, pathOut) => {
     fs.createReadStream(pathIn)
     .pipe(unzipper.Extract({ path: pathOut }));
     resolve()
-  }).then(() => {
-    console.log('Extraction Done')
-    resolve()
-  })
+  })//.then(() => {
+  //   console.log('Extraction Done')
+  //   resolve()
+  // })
 };
 
 /**
@@ -66,30 +68,131 @@ const readDir = (dir) => {
  * @return {promise}
  */
 const grayScale = (pathIn, pathOut) => {
-  // see example, barely need any changes to work
-  // DO NOT CHANGE filter type --- not allowed
-  // change rgb to make grayscale
-  // see instructions for "simple algorithms you can use for converting to grayscale"
+
+  console.log(pathIn)
+  console.log(`------------${pathOut}`)
+
   fs.createReadStream(pathIn)
-  .pipe(
-    new PNG({
-      filterType: 4,
-    })
-  )
-  .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
- 
-        // invert color
-        this.data[idx] = this.data[idx]/3;
-        this.data[idx + 1] = this.data[idx + 1]/3;
-        this.data[idx + 2] = this.data[idx + 2]/3;
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    .on("parsed", function () {
+      
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+   
+          // invert color
+          this.data[idx] = 255 - this.data[idx];
+          this.data[idx + 1] = 255 - this.data[idx + 1];
+          this.data[idx + 2] = 255 - this.data[idx + 2];
+          // console.log(this.data[idx])
+   
+          // and reduce opacity
+          this.data[idx + 3] = this.data[idx + 3] >> 1;
+        }
       }
-    }
+   
+      this.pack().pipe(fs.createWriteStream(pathOut));
+    });
+
+  
+  // var fs = require("fs"),
+  //   PNG = require("pngjs").PNG;
  
-    this.pack().pipe(fs.createWriteStream(pathOut));
-  });
+  // fs.createReadStream("in.png")
+  // .pipe(
+  //   new PNG({
+  //     filterType: 4,
+  //   })
+  // )
+  // .on("parsed", function () {
+  //   new Promise((resolve, reject) => {
+  //     for (var y = 0; y < this.height; y++) {
+  //       for (var x = 0; x < this.width; x++) {
+  //         var idx = (this.width * y + x) << 2;
+   
+  //         // invert color
+  //         // this.data[idx] = 255 - this.data[idx];
+  //         // this.data[idx + 1] = 255 - this.data[idx + 1];
+  //         // this.data[idx + 2] = 255 - this.data[idx + 2];
+   
+  //         // // and reduce opacity
+  //         // this.data[idx + 3] = this.data[idx + 3] >> 1;
+  //       }
+  //     }
+  //     // resolve()
+  //   }).then(this.pack().pipe(fs.createWriteStream("./folder000/out.png")))
+
+ 
+    // this.pack().pipe(fs.createWriteStream("out.png"));
+  // });
+  
+  
+  
+  
+  // // console.log(`${pathIn} --- ${pathOut}`)
+  // // console.log(path.extname(pathIn))
+  // // // see example, barely need any changes to work
+  // // // DO NOT CHANGE filter type --- not allowed
+  // // // change rgb to make grayscale
+  // // // see instructions for "simple algorithms you can use for converting to grayscale"
+  // console.log(`---------------------------------------------------------------------`)
+  // return new Promise((resolve, reject) => {
+  //   var fs = require("fs"),
+  //   PNG = require("pngjs").PNG;
+  //   // fs.createReadStream('./starter 4/unzipped/in.png')
+  //   fs.createReadStream("in.png")
+  //   .pipe(
+  //     new PNG({
+  //       filterType: 4,
+  //     })
+  //   )
+  //   .on("parsed", function () {
+  //     console.log('=====================================================================')
+  //     for (var y = 0; y < this.height; y++) {
+  //       for (var x = 0; x < this.width; x++) {
+  //         var idx = (this.width * y + x) << 2;
+  
+  //         // invert color
+  //         this.data[idx] = 255 - this.data[idx];
+  //         this.data[idx + 1] = 255 - this.data[idx + 1];
+  //         this.data[idx + 2] = 255 - this.data[idx + 2];
+  
+  //         // and reduce opacity
+  //         this.data[idx + 3] = this.data[idx + 3] >> 1;
+  //       }
+  //     }
+  
+  //     this.pack().pipe(fs.createWriteStream("out.png"));
+      
+  //   })
+  //   resolve()
+  // }).catch((err) => console.log(`+++++++++++++++++++++++++++++ ERROR ++++++++++++++++++++++++++++++++\n\n\n\n${err}\n\n\n+++++++++++++++++++++++++++++ END ++++++++++++++++++++++++++++++++`))
+
+
+  // fs.createReadStream(pathIn)
+  // .pipe(
+  //   new PNG({
+  //     filterType: 4,
+  //   })
+  // )
+  // .on("parsed", function () {
+  //   for (var y = 0; y < this.height; y++) {
+  //     for (var x = 0; x < this.width; x++) {
+  //       var idx = (this.width * y + x) << 2;
+ 
+  //       // invert color
+  //       this.data[idx] = this.data[idx]/3;
+  //       this.data[idx + 1] = this.data[idx + 1]/3;
+  //       this.data[idx + 2] = this.data[idx + 2]/3;
+  //     }
+  //   }
+ 
+  //   this.pack().pipe(fs.createWriteStream(pathOut));
+  // });
 };
 
 
--- starter 4/main.js
+++ starter 4/main.js
@@ -13,14 +13,32 @@ const IOhandler = require("./IOhandler"),
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
-  IOhandler.unzip('./starter 4/myfile.zip', './starter 4/unzipped').then(
+  IOhandler.unzip('./starter 4/myfile.zip', './starter 4/unzipped')
+  .then(() => {
+    // console.log('uwu')
+    console.log('Extraction Done')
     IOhandler.readDir('./starter 4/unzipped')
     // .then((png_array) => console.log(png_array))
     .then((png_array) => {
-      console.log('Files Sorted')
-      png_array.forEach(image => {
-        IOhandler.grayScale(`./starter 4/unzipped/${image}`, './starter 4/gray_scaled')
-      });
-    }
-  )
+      console.log('PNGs Pulled')
+      IOhandler.grayScale(`./starter 4/unzipped/in.png`, `./starter 4/grayscaled/test.png`)
+      // png_array.forEach(png => {
+      //   console.log(`./starter 4/unzipped/${png}`)
+      //   IOhandler.grayScale(`starter 4/unzipped/in.png`, `./starter 4/gray_scaled/${png}`)
+      // });
+      for (i=0; i < png_array.length; i++) {
+        // IOhandler.grayScale(`starter 4/unzipped/in.png`, `./starter 4/gray_scaled/test`)
+      }
+    })
+  }
+    // IOhandler.readDir('./starter 4/unzipped')
+    // .then((png_array) => console.log(png_array))
+  //   .then((png_array) => {
+  //     // console.log(png_array)
+  //     console.log('Files Sorted')
+  //     png_array.forEach(image => {
+  //       IOhandler.grayScale(`./starter 4/unzipped/${image}`, './starter 4/gray_scaled')
+  //     });
+  //   }
+  // )
 )
